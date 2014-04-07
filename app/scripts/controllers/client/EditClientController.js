3d8ccc8b0f3848bb17c593640c2a416d58c512d6
mujeeb1238 <mujeeb1238@gmail.com>
2014-04-07 18:18:00 +0530

    upload client category

--- app/scripts/controllers/client/EditClientController.js
+++ app/scripts/controllers/client/EditClientController.js
@@ -24,7 +24,7 @@
             scope.formData = {
               firstname : data.firstname,
               lastname : data.lastname,
-              clientCategory: data.clientCategory,
+              clientCategory: data.categoryType,
               middlename : data.middlename,
               active : data.active,
               externalId: data.externalId,
