70e6d83914edab459667c22bc6a2a47f33991544
hugo <ranjithworld150@gmail.com>
2014-06-09 12:01:11 +0530

    .

--- app/scripts/controllers/organization/EditEventController.js
+++ app/scripts/controllers/organization/EditEventController.js
@@ -11,9 +11,7 @@
 							scope.date = {};
 							resourceFactory.eventEditResource.get({eventId: routeParams.id} , function(data) {								
                                 scope.formData=data;   
-                                if(data.endDate){
-                                	scope.enddate = data.endDate;
-                                }
+                                scope.enddate = data.endDate;
                                 //scope.formData.allowCancellation=false;
 								scope.eventStatus = data.statusData;
 								scope.chargeData = data.chargeData;
